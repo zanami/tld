7be92e2a38a4d749788194881196702c368b7a08
Evgeny Saltykov <saltykov_ea@drom.ru>
2025-08-13 14:34:06 +0900

    module

--- footer.php
+++ footer.php
@@ -6,6 +6,7 @@ use Bitrix\Main\Localization\Loc;
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 Loc::loadMessages(__FILE__);
+?>
 
 <footer role="contentinfo">
 	<div class="w">
